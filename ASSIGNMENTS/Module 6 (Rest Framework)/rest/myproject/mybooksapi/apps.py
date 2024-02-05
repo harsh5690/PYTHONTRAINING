10489e7c1ca50ea36973ec143ee73d62a21a72e9
harsh5690 <harshmalvi5690@gmail.com>
2024-02-05 12:36:33 +0530

    update

--- ASSIGNMENTS/Module 6 (Rest Framework)/rest/myproject/mybooksapi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MybooksapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mybooksapi'
--- Assessment/Rest Framework/myrestenv/myrestproj/todoapi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('<int:pk>/',DetailToDo.as_view()),
-    path('', ListToDo.as_view()),
-    path('create',CreateToDo.as_view()),
-    path('delete/<int:pk>',DeleteToDo.as_view()),
-]
